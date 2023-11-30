7fae3c7068cea583d77229f38b69fa3c9bef5d7f
Polina-Poda <85871357+Polina-Poda@users.noreply.github.com>
2023-11-30 15:45:21 +0200

    123

--- node-app/index.js
+++ node-app/index.js
@@ -5,36 +5,30 @@ const server = require('http').createServer(app);
 const mongoose = require('mongoose');
 const port  = process.env.PORT || 4000;
 const router = require('./router')
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(router)
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended : true }));
-app.use(express.json())
 
-const cors = require('cors');
-app.options('*', cors());
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
-const corsOptions = {
-  credentials: true,
-   origin:'*',
-};
+// Connect to MongoDB using Mongoose
+mongoose.connect('mongodb+srv://aleksander:vfr4eszaq1@cluster0.jgxw19c.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
 
-app.use(cors(corsOptions));
+    // Your routes and other middleware here
 
-app.use("/",cors(corsOptions),router)
+    // Start the server
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
 
-const start = async () => {
-    try {
-      await mongoose.connect(`mongodb+srv://aleksander:vfr4eszaq1@cluster0.jgxw19c.mongodb.net/?retryWrites=true&w=majority`)
-     
-      server.listen(port, () => {
-        console.log(`Server is running on port: ${port}`);
-      });
-    } catch (e) {
-      console.log(e)
-    }
-  }
-  start();
