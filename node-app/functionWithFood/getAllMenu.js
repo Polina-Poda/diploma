e8464aedbf4f2fe2c1fdb636b20e004fbf4ed492
doct0rwho <45739838+doct0rwho@users.noreply.github.com>
2023-12-08 17:54:02 +0200

    123

--- node-app/functionWithFood/getAllMenu.js
+++ node-app/functionWithFood/getAllMenu.js
@@ -1,36 +1,30 @@
 const { Category, MenuItem } = require("../models/foodModel");
 
 async function getAllMenu(req, res) {
-    try {
-      // Отримайте всі категорії
+    try {      
       const categories = await Category.find();
-
-      // Отримайте всі страви
       const items = await MenuItem.find();
-
-      // Створіть об'єкт для збереження результатів
-      const result = {};
-
-      // Пройдіться по кожній категорії і зіберіть страви, які до неї належать
+      const result = [];
+      
       for (const category of categories) {
         const categoryName = category.name;
-
-        // Фільтруйте страви, які належать до поточної категорії
         const itemsInCategory = items.filter(
           (item) => item.category.toString() === category._id.toString()
         );
-
-        // Форматуйте страви у вказаному форматі
+      
         const formattedItems = itemsInCategory.map((item) => ({
           name: item.name,
           weight: item.weight,
           calories: item.calories,
           category: item.category,
         }));
-
-        // Додайте категорію та відформатовані страви до результату
-        result[categoryName] = formattedItems;
+      
+        result.push({
+          categoryName: categoryName,
+          items: formattedItems,
+        });
       }
+      
       return res.status(201).json({
         status: "success",
         data: result,
