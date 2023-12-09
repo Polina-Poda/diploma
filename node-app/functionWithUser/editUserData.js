ef5b24693b1fd90127304ce5f0148aca1673fd87
doct0rwho <45739838+doct0rwho@users.noreply.github.com>
2023-12-10 00:22:38 +0200

    12

--- node-app/functionWithUser/editUserData.js
+++ node-app/functionWithUser/editUserData.js
@@ -4,14 +4,6 @@ async function editUserData(req, res) {
   try {
     console.log(req.body);
     const { email, newemail , name } = req.body;
-
-    const checkNewEmail = await Users.findOne({ email: newemail });
-
-    if (checkNewEmail) {
-      return res
-        .status(402)
-        .json({ status: "error", message: "Email already exists" });
-    }
     
     await Users.updateOne({ email: email }, { $set: { userName: name, email: newemail } });
     
