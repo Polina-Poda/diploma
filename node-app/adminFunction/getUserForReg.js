328759c0fc8e0ad8b9df1fc30266b0d868959876
doct0rwho <45739838+doct0rwho@users.noreply.github.com>
2023-12-14 22:54:28 +0200

    123

--- node-app/adminFunction/getUserForReg.js
+++ node-app/adminFunction/getUserForReg.js
@@ -4,15 +4,16 @@ const { Tokens } = require("../models/tokenModel");
 async function getToken(req, res) {
   try {
     const token = req.params;
-
-    const getUser = Tokens.findOne({ token: token.token }).select("email");
+    console.log(token)
+    const getUser = await Tokens.findOne({ token: token.token })
 
     if (!getUser) {
       return res.status(404).json({
         status: "error",
-        message: "Email not found",
+        message: "token not found",
       });
     }
+    console.log(getUser.token)
 
     const checkRole = await Workers.findOne({ email: getUser.email })
 
