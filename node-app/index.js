35937e576bb795e4c7202ba8b97824ddcda67657
doct0rwho <45739838+doct0rwho@users.noreply.github.com>
2023-12-14 14:53:22 +0200

    22

--- node-app/index.js
+++ node-app/index.js
@@ -27,6 +27,14 @@ const io = require('socket.io')(server,
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(router);
 
 // Configure Socket.IO
